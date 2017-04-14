ce50d1988c668ddf1ac3d6fa45e29625b458e343
mtf <495041371@qq.com>
2017-04-14 14:21:46 +0800

    2

---
+++ js/style.js
@@ -0,0 +1,26 @@
+$(function(){
+	var index=0;
+	var time;
+//	var imgH=$('.lunbo li img:eq(0)').height();
+//	$('.lunbo li').height(imgH);
+//	$('.lunbo').height(imgH);
+//	$('ul li:eq(0)').addClass('hover')
+//	$('ul li').mouseover(function(){
+//		index=$(this).index()
+//		tab()
+//	})
+//	function tab(){
+//		$('ol li').eq(index).fadeIn(200).siblings().fadeOut(600)
+//		$('ul li').eq(index).addClass('hover').siblings().removeClass('hover')
+//	}
+//	$('#warp').hover(function(){
+//		clearInterval(time)
+//	},function(){
+//		clearInterval(time)
+//		time=setInterval(function(){
+//			tab();
+//			index++;
+//			index%=$('img').length;		
+//		},1000)
+//	}).trigger('mouseleave')
+})
