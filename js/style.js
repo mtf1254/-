569357a8dc387a64627b0fcf33d287ff6307951b
mtf <495041371@qq.com>
2017-04-17 19:42:23 +0800

    3

--- js/style.js
+++ js/style.js
@@ -1,26 +1,23 @@
 $(function(){
 	var index=0;
 	var time;
-//	var imgH=$('.lunbo li img:eq(0)').height();
-//	$('.lunbo li').height(imgH);
-//	$('.lunbo').height(imgH);
-//	$('ul li:eq(0)').addClass('hover')
-//	$('ul li').mouseover(function(){
-//		index=$(this).index()
-//		tab()
-//	})
-//	function tab(){
-//		$('ol li').eq(index).fadeIn(200).siblings().fadeOut(600)
-//		$('ul li').eq(index).addClass('hover').siblings().removeClass('hover')
-//	}
-//	$('#warp').hover(function(){
-//		clearInterval(time)
-//	},function(){
-//		clearInterval(time)
-//		time=setInterval(function(){
-//			tab();
-//			index++;
-//			index%=$('img').length;		
-//		},1000)
-//	}).trigger('mouseleave')
+	$('.lbbox .lunbobtn li:eq(0)').css('background','#66C5FF')
+	$('.lbbox .lunbobtn li').click(function(){
+		index=$(this).index()
+		tab()
+	})
+	function tab(){
+		$('.lunbo li').eq(index).fadeIn(400).siblings().fadeOut(600)
+		$('.lunbobtn li').eq(index).css('background','#66C5FF').siblings().css('background','#7E7E7E')
+	}
+	$('.lbbox').hover(function(){
+		clearInterval(time)
+	},function(){
+		clearInterval(time)
+		time=setInterval(function(){
+			tab();
+			index++;
+			index%=$('.lunbo li').length;		
+		},1000)
+	}).trigger('mouseleave')
 })
